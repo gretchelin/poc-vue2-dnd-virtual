a44c3f6893bdbe6b942ce072a03c9db860177c9a
gretchelin <gretchelin_dev@outlook.co.id>
2024-08-27 08:24:56 +0700

    build: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/*.{html,js,ts,vue}", "src/views/**/*.{html,js,ts,vue}", "src/components/**/*.{html,js,ts,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
